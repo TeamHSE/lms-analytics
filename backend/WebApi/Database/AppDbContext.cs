6bbd0fe09ceca0981f9bf3593bae131d269d7be6
namaksimow <144584830+namaksimow@users.noreply.github.com>
2024-10-25 19:25:07 +0400

    Переименование проекта

--- backend/WebApi/Database/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using WebApi.Domain;
-
-namespace WebApi.Database;
-
-public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-	public DbSet<User> Users { get; init; }
-}
